9e8713c17b025116a36b29fbc1b2d21b7acc6a2f
michael07200 <mickmhtong@gmail.com>
2022-11-12 21:21:08 +0100

    init

---
+++ ecommerce-web/src/components/News.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const News = () => {
+  return <div>
+    <div className="text-center bg-blue-500 text-white">Super Deal!</div>
+    </div>;
+};
+
+export default News;
