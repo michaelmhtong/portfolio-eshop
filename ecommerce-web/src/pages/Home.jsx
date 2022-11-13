136d33f0ff7dbd88bc129f213e3380b0572d2dc5
michael07200 <mickmhtong@gmail.com>
2022-11-13 22:33:56 +0100

    import more pages

--- ecommerce-web/src/pages/Home.jsx
+++ ecommerce-web/src/pages/Home.jsx
@@ -1,10 +1,20 @@
 import React from "react";
+import News from "../components/News";
 import Navbar from "../components/Navbar";
+import Carousel from "../components/Carousel";
+import Products from "../components/Products";
+import Newsletter from "../components/Newsletter";
+import Footer from "../components/Footer";
 
 const Home = () => {
   return (
     <div>
+      <News />
       <Navbar />
+      <Carousel />
+      <Products />
+      <Newsletter />
+      <Footer />
     </div>
   );
 };
