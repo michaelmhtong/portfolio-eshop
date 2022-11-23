84d0866b3da6b25b0734d67541f183e0a16dc3eb
michael07200 <mickmhtong@gmail.com>
2022-11-23 22:19:50 +0100

    feat: add a filter function

--- ecommerce-web/src/components/Products.jsx
+++ ecommerce-web/src/components/Products.jsx
@@ -1,13 +1,38 @@
-import React from "react";
-import { popularProducts } from "../data";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 import ProductCard from "./ProductCard";
 
-const Products = () => {
+const Products = ({ cat, filters, sort }) => {
+  const [products, setProducts] = useState([]);
+  const [filteredProducts, setFilteredProducts] = useState([]);
+
+  useEffect(() => {
+    const getProducts = async () => {
+      try {
+        const res = await axios.get(
+          // cat
+          //   ? `http://localhost:5000/api/products?category=${cat}`
+          //   : 
+          "http://localhost:5000/api/products"
+        );
+        setProducts(res.data);
+      } catch (err) {}
+    };
+    getProducts();
+  }, [cat]);
+
+  useEffect(() => {
+    cat &&
+      setFilteredProducts(
+        products.filter((item) => Object.entries(filters).every(([key, value]) => item[key].includes(value)))
+      );
+  }, [products, cat, filters]);
+
   return (
     <div className="px-16 my-10">
       <div className="text-2xl">Popular items</div>
       <div className="grid grid-cols-4 gap-4">
-        {popularProducts.map((item) => (
+        {filteredProducts.map((item) => (
           <ProductCard item={item} key={item.id} />
         ))}
       </div>
---
+++ ecommerce-web/src/pages/ProductList.jsx
@@ -0,0 +1,63 @@
+import { useState } from "react";
+import Navbar from "../components/Navbar";
+import News from "../components/News";
+import Products from "../components/Products";
+import { useLocation } from "react-router";
+
+const ProductList = () => {
+  const location = useLocation();
+  const cat = location.pathname.split("/")[2];
+  const [filters, setFilters] = useState({});
+  const [sort, setSort] = useState("newest");
+
+  const handleFilters = (e) => {
+    const value = e.target.value;
+    setFilters({
+      ...filters,
+      [e.target.name]: value,
+    });
+  };
+  console.log(filters);
+
+  const handleSort = (e) => {
+    const value = e.target.value;
+    setSort({ [e.target.name]: value });
+  };
+  console.log(sort);
+
+  return (
+    <div>
+      <div>
+        <News />
+        <Navbar />
+      </div>
+      <div>
+        <div>Men</div>
+        <div>
+          Filter:
+          <select name="size" onChange={handleFilters} className="select select-sm w-full max-w-xs">
+            <option disabled>Size</option>
+            <option>S</option>
+            <option>M</option>
+            <option>L</option>
+          </select>
+          <select name="color" onChange={handleFilters} className="select select-sm w-full max-w-xs">
+            <option disabled>Color</option>
+            <option>Black</option>
+            <option>Grey</option>
+            <option>Green</option>
+          </select>
+          <select name="sort" onChange={handleSort} className="select select-sm w-full max-w-xs">
+            <option disabled>Sort by</option>
+            <option value="newest">Newest</option>
+            <option value="asc">Price: low to high</option>
+            <option value="desc">Price: high to low</option>
+          </select>
+        </div>
+        <Products cat={cat} filters={filters} sort={sort} />
+      </div>
+    </div>
+  );
+};
+
+export default ProductList;
