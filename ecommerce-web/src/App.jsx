f6cf22f51191a7e6c87907d1ebf48c76a645fe85
michael07200 <mickmhtong@gmail.com>
2022-11-23 22:18:56 +0100

    chore: update and redirect route

--- ecommerce-web/src/App.jsx
+++ ecommerce-web/src/App.jsx
@@ -1,20 +1,22 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-
+import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
 import Home from "./pages/Home";
-import Men from "./pages/Men";
+import ProductList from "./pages/ProductList";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Cart from "./pages/Cart";
+import Product from "./pages/Product";
 
 const App = () => {
+  const user = false;
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/men" element={<Men />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
+        <Route path="/products/:category" element={<ProductList />} />
+        <Route path="/product/:id" element={<Product />} />
+        <Route path="/login" element={user ? <Navigate replace to="/" /> : <Login />} />
+        <Route path="/register" element={user ? <Navigate replace to="/" /> : <Register />} />
         <Route path="/cart" element={<Cart />} />
       </Routes>
     </Router>
