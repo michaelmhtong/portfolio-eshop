b432f9af3a3df75d54ba7ec8966f9ab6c0be7bc3
michael07200 <mickmhtong@gmail.com>
2022-11-24 21:08:36 +0100

    feat: fetch single product

--- ecommerce-web/src/pages/Product.jsx
+++ ecommerce-web/src/pages/Product.jsx
@@ -1,51 +1,80 @@
-import React from "react";
+import { useLocation } from "react-router-dom";
+import { useEffect, useState } from "react";
 import Navbar from "../components/Navbar";
 import News from "../components/News";
+import { sliderItems } from "../data";
+import { publicRequest } from "../requestMethods";
 
 const Product = () => {
+  const location = useLocation();
+  const id = location.pathname.split("/")[2];
+  const [product, setProduct] = useState({});
+  const [quantity, setQuantity] = useState(1);
+  const [color, setColor] = useState("");
+  const [size, setSize] = useState("");
+
+  useEffect(() => {
+    const getProduct = async () => {
+      try {
+        const res = await publicRequest.get("/products/find/" + id);
+        setProduct(res.data);
+      } catch {}
+    };
+    getProduct();
+  }, [id]);
+
+  const handleQuantity = (type) => {
+    if (type === "dec") {
+      quantity > 1 && setQuantity(quantity - 1);
+    } else {
+      setQuantity(quantity + 1);
+    }
+  };
+
+  const handleClick = () => {
+    //update cart
+  };
+
   return (
     <div>
       <News />
       <Navbar />
       <div className="grid grid-cols-2 gap-4 mx-60 my-10">
         <div>
-          <img
-            className=""
-            src="https://cdn.shopify.com/s/files/1/1367/5201/products/CriticalSlimZipUpHoodieBlackA1A1H9.A_ZH_GB_f42848e6-971a-410f-8171-f47c43707623_855x.jpg?v=1664781522"
-          ></img>
+          <img className="" src={product.img}></img>
         </div>
         <div>
-          <h1>Title</h1>
-          <article>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deleniti eaque provident fugit odio necessitatibus
-            accusamus natus culpa hic. Dignissimos, quasi. Iusto maiores quod dignissimos quidem consequatur ipsam nobis
-            necessitatibus! Debitis.
-          </article>
-          <h2>Price</h2>
+          <h1>{product.title}</h1>
+          <article>{product.desc}</article>
+          <h2>{product.price}</h2>
           <div className="flex items-center">
             <div className="flex items-center">
               <h3>color</h3>
-              <button className="">a</button>
-              <button className="">b</button>
-              <button className="">c</button>
+              {product.color?.map((color) => (
+                <button key={color} onClick={() => setColor(color)}>
+                  {color}
+                </button>
+              ))}
             </div>
             <div className="flex items-center">
               <h3>Size</h3>
-              <select className="select">
-                <option>S</option>
-                <option>M</option>
-                <option>L</option>
+              <select className="select" onChange={(e) => setSize(e.target.value)}>
+                {product.size?.map((size) => (
+                  <option key={size}>{size}</option>
+                ))}
               </select>
             </div>
           </div>
           <div className="flex">
             <div className="flex">
-              <button className="">-</button>
-              <h4>1</h4>
-              <button className="">+</button>
+              <button onClick={() => handleQuantity("dec")}>-</button>
+              <h4>{quantity}</h4>
+              <button onClick={() => handleQuantity("inc")}>+</button>
             </div>
             <div>
-              <button className="btn">Add to cart</button>
+              <button className="btn" onClick={handleClick}>
+                Add to cart
+              </button>
               <button className="btn">Add to wish list</button>
             </div>
           </div>
