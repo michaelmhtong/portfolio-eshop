a30cc2aa34dd5bb5c1433544b1e724d3df15e095
michael07200 <mickmhtong@gmail.com>
2022-12-26 21:38:58 +0100

    build: update filter function

---
+++ ecommerce-web/src/components/ProductsFilter.jsx
@@ -0,0 +1,92 @@
+import React, { useEffect, useState, useRef } from "react";
+import ProductCard from "./ProductCard";
+
+const Products = ({ cat, products }) => {
+  const [filters, setFilters] = useState({});
+  const [sort, setSort] = useState("newest");
+  const [filteredCat, setFilteredCat] = useState([]);
+  const [filteredProducts, setFilteredProducts] = useState([]);
+  const colorSelectRef = useRef(null);
+  const sizeSelectRef = useRef(null);
+
+  const uniqueColors = [...new Set(filteredCat.map((product) => product.color))].sort();
+  const uniqueSizes = [...new Set(filteredCat.flatMap((product) => product.size))].sort();
+
+  const handleFilters = (e) => {
+    const value = e.target.value;
+    setFilters({
+      ...filters,
+      [e.target.name]: value,
+    });
+  };
+
+  const handleSort = (e) => {
+    const value = e.target.value;
+    setSort(value);
+  };
+
+  useEffect(() => {
+    if (sort === "newest") {
+      setFilteredProducts((prev) =>
+        [...prev].sort((a, b) => new Date(a.createdAt).getTime() - new Date(b.createdAt).getTime())
+      );
+    } else if (sort === "asc") {
+      setFilteredProducts((prev) => [...prev].sort((a, b) => a.price - b.price));
+    } else if (sort === "desc") {
+      setFilteredProducts((prev) => [...prev].sort((a, b) => b.price - a.price));
+    }
+  }, [sort]);
+
+  useEffect(() => {
+    setFilteredProducts(
+      products
+        .filter((item) => item.categories.includes(cat))
+        .filter((item) => Object.entries(filters).every(([key, value]) => item[key].includes(value)))
+    );
+  }, [products, cat, filters]);
+
+  useEffect(() => {
+    setFilteredCat(products.filter((item) => item.categories.includes(cat)));
+  }, [products]);
+
+  const handleReset = () => {
+    colorSelectRef.current.value = "";
+    sizeSelectRef.current.value = "";
+    setFilters({});
+    setSort("newest");
+  };
+
+  return (
+    <div className="px-16 my-10">
+      <div>
+        Filter:
+        <select ref={colorSelectRef} name="color" onChange={handleFilters} className="select select-sm w-full max-w-xs">
+          <option disabled>Color</option>
+          {uniqueColors.map((color) => (
+            <option key={color}>{color}</option>
+          ))}
+        </select>
+        <select ref={sizeSelectRef} name="size" onChange={handleFilters} className="select select-sm w-full max-w-xs">
+          <option disabled>Size</option>
+          {uniqueSizes.map((size) => (
+            <option key={size}>{size}</option>
+          ))}
+        </select>
+        <select name="sort" onChange={handleSort} className="select select-sm w-full max-w-xs">
+          <option disabled>Sort by</option>
+          <option value="newest">Newest</option>
+          <option value="asc">Price: low to high</option>
+          <option value="desc">Price: high to low</option>
+        </select>
+        <button onClick={handleReset}>Reset</button>
+      </div>
+      <div className="grid grid-cols-4 gap-4">
+        {cat === "all"
+          ? products.map((item) => <ProductCard item={item} key={item.id} />)
+          : filteredProducts.map((item) => <ProductCard item={item} key={item.id} />)}
+      </div>
+    </div>
+  );
+};
+
+export default Products;
--- ecommerce-web/src/pages/ProductList.jsx
+++ ecommerce-web/src/pages/ProductList.jsx
@@ -1,32 +1,25 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Navbar from "../components/Navbar";
 import News from "../components/News";
-import Products from "../components/Products";
+import Products from "../components/ProductsFilter";
 import { useLocation } from "react-router";
-import ProductFilter from "../components/ProductFilter";
+import { publicRequest } from "../requestMethods";
 
 const ProductList = () => {
   const location = useLocation();
   const cat = location.pathname.split("/")[2];
   const [products, setProducts] = useState([]);
-  const uniqueColors = [...new Set(products.map((product) => product.color))].sort();
-  const uniqueSizes = [...new Set(products.flatMap((product) => product.size))].sort();
-  const [filters, setFilters] = useState([]);
-  const [sort, setSort] = useState("newest");
-
-  const handleFilters = (e) => {
-    const value = e.target.value;
-    setFilters({
-      ...filters,
-      [e.target.name]: value,
-    });
-  };
-
-  const handleSort = (e) => {
-    const value = e.target.value;
-    setSort(value);
-  };
-
+  
+  useEffect(() => {
+    const getProducts = async () => {
+      try {
+        const res = await publicRequest.get("/products");
+        setProducts(res.data);
+      } catch (err) {}
+    };
+    getProducts();
+  }, [cat]);
+ 
   return (
     <div>
       <div>
@@ -35,29 +28,7 @@ const ProductList = () => {
       </div>
       <div>
         <div>{cat}</div>
-        <div>
-          Filter:
-          {console.log("products", products)}
-          <select name="color" onChange={handleFilters} className="select select-sm w-full max-w-xs">
-            <option disabled>Color</option>
-            {uniqueColors.map((color) => (
-              <option key={color}>{color}</option>
-            ))}
-          </select>
-          <select name="size" onChange={handleFilters} className="select select-sm w-full max-w-xs">
-            <option disabled>Size</option>
-            {uniqueSizes.map((size) => (
-              <option key={size}>{size}</option>
-            ))}
-          </select>
-          <select name="sort" onChange={handleSort} className="select select-sm w-full max-w-xs">
-            <option disabled>Sort by</option>
-            <option value="newest">Newest</option>
-            <option value="asc">Price: low to high</option>
-            <option value="desc">Price: high to low</option>
-          </select>  
-        </div>
-        <Products products={products} setProducts={setProducts} cat={cat} filters={filters} sort={sort} />
+        <Products products={products} cat={cat} />
       </div>
     </div>
   );
