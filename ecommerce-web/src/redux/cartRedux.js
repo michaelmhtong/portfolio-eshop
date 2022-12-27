a5c933b20fa34e761203ccb88a9fb53b80b1e29c
michael07200 <mickmhtong@gmail.com>
2022-12-27 15:40:27 +0100

    fix: separate products with same ID but different size and color

--- ecommerce-web/src/redux/cartRedux.js
+++ ecommerce-web/src/redux/cartRedux.js
@@ -1,4 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { toast } from "react-toastify";
 
 const cartSlice = createSlice({
   name: "cart",
@@ -9,41 +10,59 @@ const cartSlice = createSlice({
   },
   reducers: {
     addProduct: (state, action) => {
-      const productIndex = state.products.findIndex((product) => product._id === action.payload._id);
-      if (productIndex >= 0) {
-        state.products[productIndex].quantity += action.payload.quantity;
-        state.quantity += action.payload.quantity;
-        state.total += action.payload.price * action.payload.quantity;
+      const existingProduct = state.products.find(
+        (product) =>
+          product._id === action.payload._id &&
+          product.color === action.payload.color &&
+          product.size === action.payload.size
+      );
+      if (existingProduct) {
+        existingProduct.quantity += action.payload.quantity;
       } else {
         state.products.push(action.payload);
-        state.quantity += action.payload.quantity;
-        state.total += action.payload.price * action.payload.quantity;
       }
+      state.quantity += action.payload.quantity;
+      state.total += action.payload.price * action.payload.quantity;
+      toast.success("Added to the cart");
     },
     increaseQuantity(state, action) {
-      const productIndex = state.products.findIndex((product) => product._id === action.payload._id);
-      state.products[productIndex].quantity += 1;
-      state.quantity += 1;
-      state.total += action.payload.price;
+      const existingProduct = state.products.find(
+        (product) =>
+          product._id === action.payload._id &&
+          product.color === action.payload.color &&
+          product.size === action.payload.size
+      );
+      if (existingProduct) {
+        existingProduct.quantity += 1;
+        state.quantity += 1;
+        state.total += action.payload.price;
+      }
     },
     decreaseQuantity(state, action) {
-      const productIndex = state.products.findIndex((product) => product._id === action.payload._id);
-      if (state.products[productIndex].quantity > 1) {
-        state.products[productIndex].quantity -= 1;
+      const existingProduct = state.products.find(
+        (product) =>
+          product._id === action.payload._id &&
+          product.color === action.payload.color &&
+          product.size === action.payload.size
+      );
+      if (existingProduct && existingProduct.quantity > 1) {
+        existingProduct.quantity -= 1;
         state.quantity -= 1;
         state.total -= action.payload.price;
       }
     },
     removeProduct(state, action) {
-      state.products.map((product) => {
-        if (product._id === action.payload._id) {
-          const nextProducts = state.products.filter((item) => item._id !== product._id);
-          state.products = nextProducts;
-          state.quantity -= action.payload.quantity;
-          state.total -= action.payload.price * action.payload.quantity;
-        }
-        return state;
-      });
+      const nextProducts = state.products.filter(
+        (product) =>
+          product._id !== action.payload._id ||
+          product.color !== action.payload.color ||
+          product.size !== action.payload.size
+      );
+      state.products = nextProducts;
+      state.quantity -= action.payload.quantity;
+      state.total -= action.payload.price * action.payload.quantity;
+      toast.success("Removed from the cart");
+      return state;
     },
     clearCart(state, action) {
       state.products = [];
